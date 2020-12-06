f31a685bc2ac19a0b3b92bb2a503025ef298a19f
zzh97228 <1002723879@qq.com>
2020-12-06 18:38:32 +0800

    feat: add build dev scripts

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 <h1 align="center">Encre</h1>
 
-Yet Another Text Editor
+Yet Another Rich Text Editor
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,7 @@ module.exports = {
   port: 8080,
   alias: {
     '/src/': resolve(__dirname, './src')
-  }
+  },
+  base: process.env.NODE_ENV === 'production' ? '/encre/' : '/',
+  outDir: resolve(__dirname, 'devDist')
 };
